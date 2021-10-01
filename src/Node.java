be6eea38d2de0b3f9641a2ce79f4813b4192c299
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-10-01 17:33:40 +0200

    Documentation

--- src/Node.java
+++ src/Node.java
@@ -1,20 +1,40 @@
 public class Node <T> {
 
+    // Next node referring to the next item in the Queue
     private Node<T> next;
+
+    // The Object this Node points to
     private final T reference;
 
+    /**
+     * Basic constructor that just takes a reference as input.
+     * No {@code next} Node needs to be specified as for a queue that Node will at first always be {@code null}
+     * @param reference the Object this Node points to
+     */
     public Node(T reference) {
         this.reference = reference;
     }
 
+    /**
+     * gets the next node specified by the {@code next} Node
+     * @return the node
+     */
     public Node<T> getNext() {
         return next;
     }
 
+    /**
+     * returns the Object this Node points to
+     * @return the reference
+     */
     public T getRef() {
         return reference;
     }
 
+    /**
+     * Sets the Next node
+     * @param next the next Node
+     */
     public void setNext(Node<T> next) {
         this.next = next;
     }
